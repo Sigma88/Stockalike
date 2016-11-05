a0bef3d9e540adc14ecc3c4c0f6e3ed68d521e27
Sigma88 <sigmamodexpansions@outlook.com>
2016-11-06 00:00:26 +0100

    recolor plugin

---
+++ [Source]/StockalikeSolarSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("StockalikeSolarSystem")]
+[assembly: AssemblyDescription("Solar system reconstructed in KSP using stockalike bodies from different mods.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sigma88")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (C) Sigma88 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: KSPAssembly("StockalikeSolarSystem", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
+[assembly: AssemblyVersion("1.0.0")]
+
