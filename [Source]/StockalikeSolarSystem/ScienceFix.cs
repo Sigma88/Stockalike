5e470c381c3ba86cf30cdfd6fef78e1f0d64f0fb
Sigma88 <sigmamodexpansions@outlook.com>
2017-05-09 23:14:40 +0200

    sciencefix

---
+++ [Source]/StockalikeSolarSystem/ScienceFix.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace SASSPlugin
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class ScienceFix : MonoBehaviour
+    {
+        void Start()
+        {
+            foreach (ConfigNode config in GameDatabase.Instance.GetConfigNodes("EXPERIMENT_DEFINITION"))
+            {
+                if (!config.HasNode("RESULTS") || !config.HasNode("STOCK_RESULTS") || !config.GetNode("STOCK_RESULTS").HasNode("RESULTS")) continue;
+                ConfigNode results = config.GetNode("RESULTS");
+                ConfigNode stockResults = config.GetNode("STOCK_RESULTS").GetNode("RESULTS");
+                string[] stockPlanets = new[] { "Sun", "Moho", "Eve", "Gilly", "Kerbin", "Mun", "Duna", "Ike", "Dres", "Jool", "Laythe", "Vall", "Tylo", "Bop", "Pol", "Eeloo" };
+
+                foreach (string planet in stockPlanets)
+                {
+                    results.RemoveValuesStartWith(planet);
+                }
+                results.AddData(stockResults);
+            }
+        }
+    }
+}
