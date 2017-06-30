d16ac970e0e14467e9b17e7898cf57a59a6b101c
Sigma88 <sigmamodexpansions@outlook.com>
2017-07-01 00:27:35 +0200

    add NodeRemover

---
+++ [Source]/StockalikeSolarSystem/NodeRemover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Linq;
+
+
+namespace SASSPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    class ConfigNodeRemover : MonoBehaviour
+    {
+        void Start()
+        {
+            foreach (UrlDir.UrlConfig node in GameDatabase.Instance.GetConfigs("StockalikeSolarSystem").Where(c => c.url != "StockalikeSolarSystem/Settings/StockalikeSolarSystem"))
+            {
+                node.parent.configs.Remove(node);
+            }
+        }
+    }
+}
