cd7e26d819a6ba985b6f3ca9fedb8f61b2e9cf1b
Sigma88 <sigmamodexpansions@outlook.com>
2018-01-28 00:38:42 +0100

    recompile

--- [Source]/StockalikeSolarSystem/Version.cs
+++ [Source]/StockalikeSolarSystem/Version.cs
@@ -6,7 +6,7 @@ namespace SASSPlugin
     [KSPAddon(KSPAddon.Startup.Instantly, true)]
     public class Version : MonoBehaviour
     {
-        public static readonly System.Version number = new System.Version("0.9.4");
+        public static readonly System.Version number = new System.Version("0.5.5");
         void Awake()
         {
             UnityEngine.Debug.Log("[SigmaLog] Version Check:   Stock-alike Solar System v" + number);
